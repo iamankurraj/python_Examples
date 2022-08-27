a89e1c6e7826263eeeb4eb7c3f0fed076152922a
iamankurraj <inaankur07@gmail.com>
2022-08-28 00:23:32 +0530

    more examples

--- 09_random_module.py
+++ 09_random_module.py
@@ -1,3 +1,8 @@
 import random
 random_integer=random.randint(1,10)
 print(random_integer)
+random_float=random.random()
+print(random_float)
+
+random_int=random.randint(1,100)
+print(f"Your number is {random_int}")
---
+++ 10_head_tail.py
@@ -0,0 +1,12 @@
+import random
+# coin_sides=random.randint(0,1)
+# if coin_sides==0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+payer=input("Enter the names\n")
+a=payer.split(", ")
+print(a)
+b=random.randint(0,len(a))
+print(a[b])
